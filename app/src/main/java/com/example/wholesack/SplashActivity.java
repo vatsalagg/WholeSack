990c57209805f057ce3d9ff09d23af6da33f08bc
vatsalagg <vatsalaggarwal1998@gmail.com>
2020-09-05 23:49:26 +0530

    hello

--- app/src/main/java/com/example/wholesack/SplashActivity.java
+++ app/src/main/java/com/example/wholesack/SplashActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        SystemClock.sleep(4000);
-        Intent loginIntent=new Intent(SplashActivity.this, RegisterActivity.class);
-        startActivity(loginIntent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(SplashActivity.this,RegisterActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
